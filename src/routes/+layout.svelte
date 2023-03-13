2024efaa5afb292fc5779bc9811f0aabb5755609
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2023-03-13 16:45:04 -0700

    Add files via upload

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Nav from '../lib/Nav.svelte';
+	import Footer from '../lib/Footer.svelte';
+</script>
+
+<body>
+	<Nav />
+	<slot />
+
+	<Footer /><!---->
+</body>
+
+<!-- svelte-ignore css-unused-selector -->
+<style lang="scss">
+	@import '../src/lib/basecamp';
+</style>
---
+++ src/routes/merch/+page.svelte
@@ -0,0 +1,36 @@
+<h1>So much stuff</h1>
+
+<div class="sales_block grid">
+	<div class="card">
+		<img src="img/test_o.jpg" alt="" />
+		<div class="card-title"><a href="/merch/shirts"> Head ware and accessories</a></div>
+		<br />
+		<div class="body">Stay warm and fancy</div>
+	</div>
+
+	<div class="card">
+		<img src="img/test_1.jpg" alt="" />
+		<div class="card-title"><a href="/merch/shirts"> Jackets and over gear</a></div>
+		<br />
+		<div class="body">Hoodies, flannels and Jackets to keep you warm and Sky High fly</div>
+	</div>
+
+	<div class="card">
+		<img src="img/test_2.jpg" alt="" />
+		<div class="card-title"><a href="/merch/shirts">Shirts</a></div>
+		<br />
+		<div class="body">Represent your homes</div>
+	</div>
+
+	<div class="card">
+		<img src="img/test_3.jpg" alt="" />
+		<div class="card-title"><a href="/merch/shirts"> Pants & Sweats</a></div>
+		<br />
+		<div class="body">Be cozy and the best looking in the room at the same time.</div>
+	</div>
+</div>
+
+<!-- svelte-ignore css-unused-selector -->
+<style lang="scss">
+	@import '../src/lib/basecamp';
+</style>
